630816f895789846aacc7a868c635fc8a57705a9
igizxabibullin <ilgizxabibullin@yandex.ru>
2023-11-05 15:47:17 +0500

    Удаление параметра sleep(3)

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -16,7 +16,6 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         configurationForConnecting connectingSceneSession: UISceneSession,
         options: UIScene.ConnectionOptions
     ) -> UISceneConfiguration {
-        sleep(3)
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
